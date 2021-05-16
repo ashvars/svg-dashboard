aecb6d945a5c84c2a6cb2fab28493fc703207e23
ashvars <ashvars@gmail.com>
2021-05-16 14:45:37 +1000

    update file rename op from async to sync

--- server.js
+++ server.js
@@ -8,35 +8,45 @@ const upload = multer({dest: __dirname + '/uploads/images'});
 const app = express();
 const PORT = 3000;
 
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 app.use(express.static('public'));
 
-app.post('/upload', upload.array('photo'), (req, res) => {
+app.post('/upload', upload.array('photo'), async (req, res) => {
 
-    let svgs = '';
+    let svgs = '', count = 0;
 
     fsExtra.emptyDirSync('icons');
 
     if(req.files) {
-        for(var i = 0;i < req.files.length;i++) {
-            const tempPath = req.files[i].path;
-            const originalName = req.files[i].originalname;
+        for(var f of req.files) {
+            const tempPath = f.path;
+            const originalName = f.originalname;
             const targetPath = path.join(__dirname, "./icons/" + originalName);
-            fs.rename(tempPath, targetPath, err => {});
-            console.log(`${tempPath}    ->    ${originalName}`);
+            fs.renameSync(tempPath, targetPath, (err) => { err && console.log(`Error in file rename : ${err}`); });
         }
+        //await snooze(3000);
         file.recurseSync("icons/", [
             '*.svg'
             ], function(filepath, relative, filename) {
+            count ++;
             if (!filename) {
                 return;
             }
             var content = file.readFileSync(filepath, {encoding: 'utf-8'});
-            svgs += `<div title="${filename}">${content}</div>`;
+            svgs += `<div class="svg" title="${filename}">${content}<label>${filename}</label></div>`;
         });
+        console.log(`After appending ${count} svgs to variable`);
         file.writeFileSync('final.html', `
             <!DOCTYPE html>
             <html>
-                <head><style>svg { height: 50px; width: auto; margin: 10px; } #container { display: flex; flex-wrap: wrap; }</style></head>
+                <head>
+                    <style>
+                        svg { height: 50px; width: auto; margin: 10px; }
+                        #container { display: flex; flex-wrap: wrap; }
+                        .svg { display: flex; flex-direction: column; margin: 20px 10px; }
+                    </style>
+                </head>
                 <body>
                     <div>
                         <button onclick="window.history.back();">Back to Home</button>
